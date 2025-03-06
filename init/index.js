4c7c3a2eaa952660cd0b595d6d9eb226a30418a0
vaibhavPrasad <vaibhavprasad@gmail.com>
2025-03-06 23:21:36 +0530

    added owner

--- init/index.js
+++ init/index.js
@@ -13,6 +13,7 @@ async function main(){
 
 const initDB = async ()=>{
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) =>({...obj, owner: '67c9aa722df9e98604ead6a8'}));
     await Listing.insertMany(initData.data);
     console.log("data inserted");
 };
