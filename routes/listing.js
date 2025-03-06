d73b9d66f633f7e409c7161234110305e9295c04
vaibhavPrasad <vaibhavprasad@gmail.com>
2025-03-06 23:53:10 +0530

    showed owner in show.ejs

--- routes/listing.js
+++ routes/listing.js
@@ -33,7 +33,7 @@ router.get("/new",isLoggedIn,(req, res)=>{
 //read,show route
 router.get("/:id",wrapAsync( async (req, res)=>{
     let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing){
         req.flash("error", "Listing You Requested Does Not Exist!");
         res.redirect("/listings");
@@ -45,6 +45,7 @@ router.get("/:id",wrapAsync( async (req, res)=>{
 //create route
 router.post("/",isLoggedIn,validateListing,wrapAsync(async (req, res)=>{
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
