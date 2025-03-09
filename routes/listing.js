cce4f2884801459886531e58bcba3d165ce71929
vaibhavPrasad <vaibhavprasad@gmail.com>
2025-03-09 12:42:42 +0530

    1.added in a form of new.ejs enctype='multipart/form-data' 2. installed a npm package name npm i multer for accepting multipart/form-data 3. added a .env file and downloaded a npm package name npm i dotenv 4. installed a npm package name 1. npm i cloudinary & 2. npm i multer-storage for upload photo on cloudinary 3rd party api

--- routes/listing.js
+++ routes/listing.js
@@ -5,14 +5,18 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 const controllers = require("../controllers/listings.js");
-
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 //index route
 //create route
 router.route("/")
 .get(wrapAsync(controllers.index))
-.post(isLoggedIn,validateListing,wrapAsync(controllers.createListing));
-
+// .post(isLoggedIn,validateListing,wrapAsync(controllers.createListing));
+.post(upload.single("listing[image]"),(req,res)=>{
+    res.send(req.file);
+})
 
 //new route
 router.get("/new",isLoggedIn,controllers.renderNewForm);
